537871d94b7f7227f2f9fc2f6719e4114800c251
Mustafa Hıncal <mustafahncal@gmail.com>
2022-03-16 21:09:46 +0300

    all files updated

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Javascript Quiz Uygulaması
+# Quiz App
 
-![](./screenshots/app.jpg)
+![](./gif/quizapp.quizapp.gif)
+
+## Used Technologies
+
+-   HTML
+-   CSS
+-   Javascript
+
+## Vercel
+
+Here's how it looks in Vercel : [ Quiz App](https://quiz-app-id2il082s-mustafahincal.vercel.app/)
--- app.js
+++ app.js
@@ -30,12 +30,28 @@ class Quiz {
     }
 }
 
-var q1 = new Question("what's the best programming language?", ["C#", "Javascript", "Python", "Java"], "Javascript");
-var q2 = new Question("what's the most popular language?", ["C++", "Javascript", "Delphi", "Java"], "Javascript");
-var q3 = new Question("what's the best modern programming language?", ["Nodejs", "Javascript", "Python", "Visual Basic"], "Javascript");
+let q1 = new Question(
+    "What's the best programming language?",
+    ["C#", "Javascript", "Python", "Java"],
+    "Javascript"
+);
+let q2 = new Question(
+    "What's the most popular language?",
+    ["C++", "Javascript", "Delphi", "Java"],
+    "Javascript"
+);
+let q3 = new Question(
+    "What's the best modern programming language?",
+    ["C#", "Javascript", "Python", "Visual Basic"],
+    "Javascript"
+);
+let q4 = new Question(
+    "What's the best javascript framework?",
+    ["Vue.js", "React", "Angular", "Node.js"],
+    "React"
+);
 
-let questions = [q1, q2, q3];
-
+let questions = [q1, q2, q3, q4];
 
 var quiz = new Quiz(questions);
 
@@ -43,13 +59,22 @@ loadQuestion();
 
 function loadQuestion() {
     if (quiz.isFinish()) {
-        result = `<h1>Your Score</h1><h2>${quiz.score}</h2>`;
+        result = `
+                <div>
+                <h2>Correct Answer : ${quiz.score}</h2>
+                <h2>Wrong Answer : ${quiz.questions.length - quiz.score}</h2>
+                <h1 class="score">Your Score : ${
+                    (100 / quiz.questions.length) * quiz.score
+                }</h1>
+                </div>
+                `;
         document.querySelector("#body").innerHTML = result;
     } else {
         var question = quiz.getQuestion();
         var choices = question.choices;
 
-        document.querySelector("#question").textContent = question.text;
+        document.querySelector("#question").textContent =
+            quiz.questionIndex + 1 + " ) " + question.text;
 
         for (i = 0; i < choices.length; i++) {
             var button = document.querySelector("#btn" + i);
@@ -63,14 +88,16 @@ function loadQuestion() {
 
 function guess(id, guess) {
     var btn = document.getElementById(id);
-    btn.onclick = function() {
+    btn.onclick = function () {
         quiz.guess(guess);
         loadQuestion();
-    }
+    };
 }
 
 function showProgress() {
     var totalQuestion = quiz.questions.length;
     var questionNumber = quiz.questionIndex + 1;
-    document.querySelector("#footer").textContent = `Question ${questionNumber} of ${totalQuestion}`;
+    document.querySelector(
+        "#footer"
+    ).textContent = `Question ${questionNumber} of ${totalQuestion}`;
 }
